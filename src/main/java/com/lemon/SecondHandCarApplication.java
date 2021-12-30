b8df41221ca15055eba6f02897783c0dfb3d24f2
lemon <806416409@qq.com>
2021-12-30 15:53:03 +0800

    update

---
+++ src/main/java/com/lemon/SecondHandCarApplication.java
@@ -0,0 +1,13 @@
+package com.lemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandCarApplication.class, args);
+    }
+
+}
